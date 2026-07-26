e515dede57c7b09c9d1d4c5f6c430991154c304c
kilash12 <kailashkumar975954@gmail.com>
2026-07-26 14:02:54 +0530

    Allow Render frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://taskmanagement-frontend.onrender.com",
     "https://taskmanagement-l10msyeey-kilash12s-projects.vercel.app",
 ]
 
@@ -84,6 +85,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://taskmanagement-frontend.onrender.com",
     "https://taskmanagement-l10msyeey-kilash12s-projects.vercel.app",
 ]
 
