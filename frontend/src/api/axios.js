370e3827378a1bb7cf9d84fd377ef5dd8d7761eb
kilash12 <kailashkumar975954@gmail.com>
2026-07-26 11:31:51 +0530

    Update backend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,25 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: "http://localhost:8000/api",
+//     baseURL: "https://taskmanagement-backend-es89.onrender.com/api",
+//     withCredentials: true,
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+// });
+
+// export default api;
+
 import axios from "axios";
 
+const baseURL =
+    window.location.hostname === "localhost"
+        ? "http://localhost:8000/api"
+        : "https://taskmanagement-backend-es89.onrender.com/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
